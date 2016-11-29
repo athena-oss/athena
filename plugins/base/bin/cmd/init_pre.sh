c79df421035c56e4b57547acb12e3ccab408cd64
Rafael Pinto <rafael.pinto@olx.com>
2016-11-29 16:44:46 +0100

    Removed unnecessary echos

--- plugins/base/bin/cmd/init_pre.sh
+++ plugins/base/bin/cmd/init_pre.sh
@@ -7,24 +7,20 @@ if ! which git 1>/dev/null 2>/dev/null ; then
 fi
 athena.ok "git is installed."
 
-echo
 if ! which docker 1>/dev/null 2>/dev/null ; then
 	athena.fatal "docker is not installed! Please install docker."
 fi
 athena.ok "docker is installed."
 
-echo
 ip=$(athena.docker.get_ip)
 if athena.os.is_mac && [[ "$ip" != "127.0.0.1" ]]; then
 	# this means docker-machine is being used instead of the native version (docker for mac)
 	if [ -z "$DOCKER_MACHINE_NAME" ]; then
-		echo
 		msg=$(printf "docker environment is not setted! Please start docker-machine and run 'eval \$(docker-machine env default)'.")
 		athena.fatal "$msg"
 	fi
 fi
 
-echo
 if ! athena.docker ps 1>/dev/null; then
 	athena.fatal "docker is not running! Please start docker daemon."
 fi
@@ -37,8 +33,6 @@ fi
 athena.ok "all called functions exist (athena built-in only)."
 
 echo "============================================================="
-echo
 athena.print "green" "All systems are GO. Enjoy!"
-echo
 athena.os._check_athena_in_path "$SHELL"
 echo "============================================================="
